25559166cdfe1707bf86f4a2595234dfb7cf45d8
marius <cepulameavrei>
2019-03-21 13:01:01 +0100

    implemented dynamic quiz creation

--- src/app/create-quiz/create-quiz.component.ts
+++ src/app/create-quiz/create-quiz.component.ts
@@ -1,5 +1,6 @@
+/* tslint:disable:no-angle-bracket-type-assertion */
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SNACKBAR_TIMEOUT } from '../entities/globals';
 import { MatSnackBar } from '@angular/material';
 import { Router } from '@angular/router';
@@ -16,33 +17,40 @@ export class CreateQuizComponent implements OnInit {
   quizForm: FormGroup;
   users: User[];
 
-  constructor(private fb: FormBuilder, private  snackBar: MatSnackBar, private router: Router, private tempData: TempDataService) {
-    this.users = this.tempData.getUsers();
+  constructor(private fb: FormBuilder, private  snackBar: MatSnackBar, private router: Router, private data: TempDataService) {
+    this.users = this.data.getUsers();
   }
 
   ngOnInit() {
     this.quizForm = this.fb.group(
       {
-        _id: [''],
-        visible: [''],
         title: ['', [Validators.required, Validators.minLength(3)]],
-        userId: ['', Validators.required],
-        q1Name: ['', [Validators.required, Validators.minLength(3)]],
-        q1A1Title: ['', Validators.required],
-        q1A1IsCorrect: [''],
-        q1A2Title: ['', Validators.required],
-        q1A2IsCorrect: [''],
-        q1A3Title: ['', Validators.required],
-        q1A3IsCorrect: [''],
-        q2Name: ['', [Validators.required, Validators.minLength(3)]],
-        q2A1Title: ['', Validators.required],
-        q2A1IsCorrect: [''],
-        q2A2Title: ['', Validators.required],
-        q2A2IsCorrect: [''],
-        q2A3Title: ['', Validators.required],
-        q2A3IsCorrect: ['']
+        user: ['', Validators.required],
+        questions: this.fb.array([])
       }
     );
+    this.newQuestion();
+  }
+
+  newQuestion() {
+    const question = this.fb.group({
+      title: ['', Validators.required],
+      options: this.fb.array([])
+    });
+
+    const questions = this.quizForm.controls.questions as FormArray;
+    const options = question.controls.options as FormArray;
+    options.push(this.newOptionGroup());
+    options.push(this.newOptionGroup());
+
+    questions.push(question);
+  }
+
+  newOption(questionIndex: number) {
+    const option = this.newOptionGroup();
+    const questions = this.quizForm.controls.questions as FormArray;
+    const options = (<FormArray> questions.controls[questionIndex]).controls['options'] as FormArray;
+    options.push(option);
   }
 
   onSubmit() {
@@ -51,60 +59,34 @@ export class CreateQuizComponent implements OnInit {
       duration: SNACKBAR_TIMEOUT,
     });
 
-    this.quizForm.value._id = String(this.tempData.getQuizzes().length);
+    this.quizForm.value._id = String(this.data.getQuizzes().length);
     this.quizForm.value.visible = true;
+    this.quizForm.value.created = new Date();
 
     if (this.quizForm.valid) {
 
-      const quiz: Quiz = {
-        _id: this.quizForm.value._id,
-        visible: this.quizForm.value.visible,
-        user: this.tempData.findUser(this.quizForm.value.userId),
-        title: this.quizForm.value.title,
-        created: new Date(),
-        questions: [
-          {
-            title: this.quizForm.value.q1Name,
-            options: [
-              {
-                answer: this.quizForm.value.q1A1Title,
-                correct: this.quizForm.value.q1A1IsCorrect
-              },
-              {
-                answer: this.quizForm.value.q1A2Title,
-                correct: this.quizForm.value.q1A2IsCorrect
-              },
-              {
-                answer: this.quizForm.value.q1A3Title,
-                correct: this.quizForm.value.q1A3IsCorrect
-              }
-            ]
-          },
-          {
-            title: this.quizForm.value.q2Name,
-            options: [
-              {
-                answer: this.quizForm.value.q2A1Title,
-                correct: this.quizForm.value.q2A1IsCorrect
-              },
-              {
-                answer: this.quizForm.value.q2A2Title,
-                correct: this.quizForm.value.q2A2IsCorrect
-              },
-              {
-                answer: this.quizForm.value.q2A3Title,
-                correct: this.quizForm.value.q2A3IsCorrect
-              }
-            ]
-          }
-        ]
-      };
+      // const quiz: Quiz = {
+      //   _id: this.quizForm.value._id,
+      //   visible: this.quizForm.value.visible,
+      //   user: this.data.findUser(this.quizForm.value.userId),
+      //   title: this.quizForm.value.title,
+      //   created: new Date(),
+      //   questions: []
+      // };
 
-      this.tempData.addQuiz(quiz);
+      this.data.addQuiz(this.quizForm.value as Quiz);
+      // this.data.addQuiz(this.quizForm.values as Quiz);
 
-      console.log('saving quiz id:%s by "%s"', quiz._id, quiz.user.username);
+      // console.log('saving quiz id:%s by "%s"', quiz._id, quiz.user.username);
 
       this.router.navigate(['portal/display-quizzes']);
     }
+  }
+
+  private newOptionGroup() {
+    return this.fb.group({
+      answer: ['', Validators.required],
+      correct: [false, Validators.required]
+    });
   }
 }
