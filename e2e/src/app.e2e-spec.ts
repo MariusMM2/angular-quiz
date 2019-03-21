d7d0c4214f4d7f8e1923db239efc59d0e8d588dd
marius <cepulameavrei>
2019-03-21 14:28:30 +0100

    added 2 e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, by, element, logging } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -8,9 +8,23 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to Quiz!');
+  // 1.0: Verify that I can go to the login component
+  // 1.1: ...
+
+  it('1.0: Verify that I can go to the login component', () => {
+    browser.get('home/login');
+    let loginText = element(by.id('login')).getText();
+    expect(loginText).toEqual('Login');
+
+  });
+
+  it('1.1: Verify that i can login and be redirected to display-quizzes', function() {
+    element(by.id("username")).sendKeys("admin");
+    element(by.id("password")).sendKeys("admin");
+    element(by.id("submitBtn")).click();
+
+    let quizCard = element.all(by.css(".quiz-card")).get(0);
+    expect(quizCard.isPresent());
   });
 
   afterEach(async () => {
